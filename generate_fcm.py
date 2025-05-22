49fb2d4b49498f4384e283e2edbe634381d96e3d
Ꮛnѵoყ-Z <envoyzlab@gmail.com>
2025-05-22 16:48:54 +0530

    Update generate_fcm.py

--- generate_fcm.py
+++ generate_fcm.py
@@ -1,129 +1,117 @@
 # Author: Sebastiano Barezzi <barezzisebastiano@gmail.com>
 # Modifier: Envoy-Z-Lab <envoyzlab@gmail.com>
-# Version: 2.0
+# Version: 1.5
 
 from re import search
-from typing import Dict
-import sys
-
 
 class Version:
     def __init__(self, version: str):
         self.major, self.minor = version.split(".")
-        self.major = int(self.major)
-        self.minor = int(self.minor)
 
-    def merge_version(self, other):
-        if other.minor > self.minor:
-            self.minor = other.minor
+    def merge_version(self, version):
+        if version.minor > self.minor:
+            self.minor = version.minor
 
-    def format(self) -> str:
+    def format(self):
         version_str = '    <version>'
-        if self.minor > 0:
+        if int(self.minor) > 0:
             version_str += f"{self.major}.0-{self.minor}"
         else:
-            version_str += f"{self.major}.0"
+            version_str += f"{self.major}.{self.minor}"
         version_str += '</version>\n'
+
         return version_str
-
-    def __str__(self):
-        return f"{self.major}.{self.minor}"
-
 
 class Interface:
     def __init__(self, name: str, instance: str):
         self.name = name
         self.instances = [instance]
 
-    def merge_interface(self, other):
-        for instance in other.instances:
+    def merge_interface(self, interface):
+        for instance in interface.instances:
             if instance not in self.instances:
-                self.instances.append(instance)
+                self.instances += [instance]
 
-    def format(self) -> str:
-        result = '    <interface>\n'
-        result += f'        <name>{self.name}</name>\n'
-        for instance in sorted(self.instances):
-            result += f'        <instance>{instance}</instance>\n'
-        result += '    </interface>\n'
-        return result
+    def format(self):
+        interface_str = '    <interface>\n'
+        interface_str += f'        <name>{self.name}</name>\n'
+        for instance in self.instances:
+            interface_str += f'        <instance>{instance}</instance>\n'
+        interface_str += '    </interface>\n'
 
+        return interface_str
 
 class Entry:
     def __init__(self, fqname: str):
         self.type = "HIDL" if "@" in fqname else "AIDL"
 
         if self.type == "HIDL":
-            self.name, version_str = fqname.split("::")[0].split("@")
-            interface_part = fqname.split("::")[1]
-            interface_name, instance = interface_part.split("/", 1)
-            version = Version(version_str)
-            self.versions: Dict[int, Version] = {version.major: version}
+            self.name, version = fqname.split("::")[0].split("@")
+            interface_name, interface_instance = fqname.split("::")[1].split("/", 1)
+            version = Version(version)
+            self.versions = {version.major: version}
         else:
-            self.name, iface_str = fqname.rsplit(".", 1)
-            interface_name, instance = iface_str.split("/", 1)
+            self.name, interface_str = fqname.rsplit(".", 1)
+            interface_name, interface_instance = interface_str.split("/")
             self.versions = {}
 
-        interface = Interface(interface_name, instance)
-        self.interfaces: Dict[str, Interface] = {interface.name: interface}
+        interface = Interface(interface_name, interface_instance)
+        self.interfaces = {interface.name: interface}
 
-    def merge_entry(self, other):
-        if self.name != other.name or self.type != other.type:
-            raise AssertionError("Mismatched entry during merge.")
+    def merge_entry(self, entry):
+        if entry.name != self.name:
+            raise AssertionError("Different entry name")
 
-        for major, version in other.versions.items():
-            if major in self.versions:
-                self.versions[major].merge_version(version)
+        if entry.type != self.type:
+            raise AssertionError("Different HAL type")
+
+        for version_major, version in entry.versions.items():
+            if version_major in self.versions:
+                self.versions[version_major].merge_version(version)
             else:
-                self.versions[major] = version
+                self.versions[version_major] = version
 
-        for name, interface in other.interfaces.items():
-            if name in self.interfaces:
-                self.interfaces[name].merge_interface(interface)
+        for interface_name, interface in entry.interfaces.items():
+            if interface_name in self.interfaces:
+                self.interfaces[interface_name].merge_interface(interface)
             else:
-                self.interfaces[name] = interface
+                self.interfaces[interface_name] = interface
 
-    def format(self) -> str:
-        result = f'<hal format="{self.type.lower()}" optional="true">\n'
-        result += f'    <name>{self.name}</name>\n'
-        for version in sorted(self.versions.values(), key=lambda v: v.major):
-            result += version.format()
-        for interface in sorted(self.interfaces.values(), key=lambda i: i.name):
-            result += interface.format()
-        result += '</hal>\n'
-        return result
+    def format(self):
+        entry_str = f'<hal format="{self.type.lower()}" optional="true">\n'
+        entry_str += f'    <name>{self.name}</name>\n'
 
+        for version in self.versions.values():
+            entry_str += version.format()
+
+        for interface in self.interfaces.values():
+            entry_str += interface.format()
+
+        entry_str += '</hal>\n'
+        return entry_str
 
 def main():
-    input_file = "fqnames.txt"
-    entries: Dict[str, Entry] = {}
+    entries = {}
+    for fqname in open("fqnames.txt").readlines():
+        fqname = fqname.strip()
 
-    with open(input_file, "r") as f:
-        for line in f:
-            fqname = line.strip()
-            if not fqname or fqname.startswith("#"):
-                continue
+        if fqname == "" or fqname[0] == '#':
+            continue
 
-            match = search(r" @\d+$", fqname)
-            if match:
-                fqname = fqname.removesuffix(match.group(0))
+        versioned_aidl_match = search(" @[0-9]+$", fqname)
+        if versioned_aidl_match:
+            fqname = fqname.removesuffix(versioned_aidl_match.group(0))
 
-            entry = Entry(fqname)
-            key = f"{entry.type}:{entry.name}"
+        entry = Entry(fqname)
+        entry_key = f"{entry.type}:{entry.name}"
+        if entry_key in entries:
+            entries[entry_key].merge_entry(entry)
+        else:
+            entries[entry_key] = entry
 
-            if key in entries:
-                entries[key].merge_entry(entry)
-            else:
-                entries[key] = entry
+    fcms = [entry.format() for entry in entries.values()]
+    print('<compatibility-matrix version="2.0" type="framework">')
+    print("".join(fcms))
+    print('</compatibility-matrix>')
 
-    output = '<?xml version="1.0" encoding="utf-8"?>\n'
-    output += '<compatibility-matrix version="2.0" type="framework">\n'
-    for entry in sorted(entries.values(), key=lambda e: e.name):
-        output += entry.format()
-    output += '</compatibility-matrix>\n'
-
-    print(output)
-
-
-if __name__ == "__main__":
-    main()
+main()
